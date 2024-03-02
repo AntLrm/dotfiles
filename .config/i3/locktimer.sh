1042cae01efc9e50d8986647a1f4c00249348f6d
Antoine <antoinelormeau@gmail.com>
2024-03-02 09:22:45 +0100

    Change

--- .config/i3/locktimer.sh
+++ .config/i3/locktimer.sh
@@ -7,15 +7,15 @@ export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 xidlehook \
   --not-when-fullscreen \
   --not-when-audio \
-  --timer 20 \
+  --timer 60 \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness .2' \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
   --timer 300 \
     'xrandr --output "$PRIMARY_DISPLAY" --brightness .7; i3lock -i ~/.config/i3/wallpaper.png -t -ef -n' \
-    '' \
+    'xrandr --output "$PRIMARY_DISPAY" --brightness 1' \
   --timer 10 \
     'xset dpms force off' \
-    '' \
+    'xrandr --output "$PRIMARY_DISPAY" --brightness 1' \
   --timer 2000 \
     'systemctl suspend' \
-    ''
+    'xrandr --output "$PRIMARY_DISPAY" --brightness 1' \
